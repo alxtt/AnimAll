0144c881bef99b471c182c2c905d27f8de1f0483
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2021-12-25 01:45:24 +0300

    Update __init__.py

--- api/HttpTrigger/__init__.py
+++ api/HttpTrigger/__init__.py
@@ -1,24 +1,15 @@
+import datetime
 import logging
-
+import os
+import requests
+import json
+import pyodbc
+from datetime import datetime as dt
 import azure.functions as func
 
+AZURE_CONN_STRING = str(os.environ["AzureSQLConnectionString"])
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    result = {}
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(json.dumps(result))
