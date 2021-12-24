a92a7b5a03516b0f74c65a69e6c0bfaf8d2a5e2e
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2021-12-25 01:52:46 +0300

    Update __init__.py

--- api/HttpTrigger/__init__.py
+++ api/HttpTrigger/__init__.py
@@ -10,6 +10,20 @@ import azure.functions as func
 AZURE_CONN_STRING = str(os.environ["AzureSQLConnectionString"])
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    result = {}
+        conn = pyodbc.connect(AZURE_CONN_STRING)
+        
+        with conn.cursor() as cursor:
+            cursor.execute(f"EXEC [web].[GetMonitoredBusData] ?, ?", rid, gid)
+
+            result = cursor.fetchone()[0]
+            
+            if result:
+                result = json.loads(result)                           
+            else:
+                result = {}     
+
+            logging.info(result)   
+        
+        cursor.close()
 
     return func.HttpResponse(json.dumps(result))
