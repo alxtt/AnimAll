643dcfc373f045e9ac998e7f9ba5daf0d89cf08e
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2021-12-26 17:56:04 +0300

    meh

--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -9,11 +9,6 @@ const PostFilter = ({filter, setFilter}) => {
                 onChange={e => setFilter({...filter, query: e.target.value})}
                 placeholder="Search"
             />
-            <MyInput
-                value={filter.query}
-                onChange={e => setFilter({...filter, query: e.target.value})}
-                placeholder="Search"
-            />
         </div>
     );
 };
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -22,16 +22,16 @@ const PostForm = ({create}) => {
                 value={post.title}
                 onChange={e => setPost({...post, title: e.target.value})}
                 type="text"
-                placeholder="Название поста"
+                placeholder="Name"
             />
             {/*Неуправляемый\Неконтролируемый компонент*/}
             <MyInput
                 value={post.body}
                 onChange={e => setPost({...post, body: e.target.value})}
                 type="text"
-                placeholder="Описание поста"
+                placeholder="Breed, location"
             />
-            <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            <MyButton onClick={addNewPost}>Create</MyButton>
         </form>
     );
 };
