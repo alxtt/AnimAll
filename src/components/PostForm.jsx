66294b5151773504c4e8067e7b0329ac8685aca3
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2021-12-27 06:23:54 +0300

    Update PostForm.jsx

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,7 +3,7 @@ import MyInput from "./UI/input/MyInput";
 import MyButton from "./UI/button/MyButton";
 
 const PostForm = ({create}) => {
-    const [post, setPost] = useState({title: '', body: ''})
+    const [post, setPost] = useState({title: '', subtitle: '', body: '', url: ''})
 
 
     const addNewPost = (e) => {
@@ -12,24 +12,34 @@ const PostForm = ({create}) => {
             ...post, id: Date.now()
         }
         create(newPost)
-        setPost({title: '', body: ''})
+        setPost({title: '', subtitle: '', body: '', url: ''})
     }
 
     return (
         <form>
-            {/*Управляемый компонент*/}
             <MyInput
                 value={post.title}
                 onChange={e => setPost({...post, title: e.target.value})}
                 type="text"
                 placeholder="Name"
             />
-            {/*Неуправляемый\Неконтролируемый компонент*/}
+            <MyInput
+                value={post.subtitle}
+                onChange={e => setPost({...post, subtitle: e.target.value})}
+                type="text"
+                placeholder="Breed, location"
+            />
             <MyInput
                 value={post.body}
                 onChange={e => setPost({...post, body: e.target.value})}
                 type="text"
-                placeholder="Breed, location"
+                placeholder="Description"
+            />
+            <MyInput
+                value={post.url}
+                onChange={e => setPost({...post, url: e.target.value})}
+                type="text"
+                placeholder="Image url"
             />
             <MyButton onClick={addNewPost}>Create</MyButton>
         </form>
